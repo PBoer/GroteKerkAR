013040275c78f1efc87a7342c5c0f2f29b91a724
PBoer <pasc.boer@gmail.com>
2018-04-26 15:42:47 +0200

    architect game dialogue
    added dialogue to start of game

---
+++ Grote Kerk/Assets/Scripts/DialogueButtonFunctions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueButtonFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SkipDialogue()
+    {
+        GameObject.Find("DialogueCanvas").SetActive(false);
+    }
+
+    public void ForwardDialogue()
+    {
+
+    }
+}
---
+++ Grote Kerk/Assets/Scripts/DialogueScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueScript : MonoBehaviour {
+
+    public float letterPause = 0.2f;
+    public AudioClip sound;
+
+    private GameObject dialogueCanvas;
+    private int dialogueCount;
+    private List<string> messages;
+    private string message;
+    private Text myText;
+
+    // Use this for initialization
+    void Start()
+    {
+        dialogueCanvas = GameObject.Find("DialogueCanvas");
+        dialogueCount = 0;
+        CreateDialogue();
+        myText = GetComponent<Text>();
+        myText.text = "";
+        ForwardDialogue();
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            myText.text += letter;
+            if (sound)
+                GetComponent<AudioSource>().PlayOneShot(sound);
+            yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+
+    public void SkipDialogue()
+    {
+        dialogueCanvas.SetActive(false);
+    }
+
+    public void ForwardDialogue()
+    {
+        switch (GameManager.Instance.GetCurrentScene())
+        {
+            case "ArchitectGame":
+                if (dialogueCount <= 2)
+                {
+                    myText.text = "";
+                    message = messages[dialogueCount];
+                    dialogueCount++;
+                    StopAllCoroutines();
+                    StartCoroutine(TypeText());
+                }
+                else
+                {
+                    SkipDialogue();
+                }
+                break;
+        }
+    }
+
+    // Create array with dialogue messages
+    // Keep messages at 230 or less characters
+    public void CreateDialogue()
+    {
+        messages = new List<string>
+        {
+            "Test 1 hallo ik ben Heinrich",
+            "Test 2",
+            "Test 3",
+            "Gefeliciteerd!"
+        };
+    }
+}
