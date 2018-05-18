0dd60b7808acc345654026dda734c434d5b4e024
Heemskerkp3 <s1095111@student.windesheim.nl>
2018-05-18 13:49:37 +0200

    Timeline done one 1 thing left but that is only possible after the history points

--- Grote Kerk/Assets/Scripts/TimelineFunctions.cs
+++ Grote Kerk/Assets/Scripts/TimelineFunctions.cs
@@ -6,11 +6,38 @@ using UnityEngine.UI;
 
 public class TimelineFunctions : MonoBehaviour {
 
-    public GameObject Content;
+    public GameObject moreInfoPanel;
+    public GameObject moreInfoText;
+    public GameObject mainPanel;
+    public List<TextAsset> Assets;
+
+    void Start()
+    {
+        int count = 1;
+        foreach (TextAsset asset in Assets)
+        {
+            //TODO uncomment with 
+            //if(PlayerPrefs.GetInt("HistoryPoint"+ count) == 1){
+            mainPanel.transform.Find("HistoryPoint" + count).Find("HistoryText").GetComponent<Text>().text = asset.text;
+            //}
+            //else{
+            //mainPanel.transform.Find("HistoryPoint" + count).Find("HistoryText").GetComponent<Text>().text = "Scan het geschiedenispunt om dit vrij te spelen";
+            //}
+            count++;
+        }
+
+        
+    }
+
+
     public void moreInfo(int historyPointID)
     {
-           
-        
+        moreInfoText.GetComponent<Text>().text = mainPanel.transform.Find("HistoryPoint" + historyPointID).Find("HistoryText").GetComponent<Text>().text;
+        moreInfoPanel.SetActive(true); 
+    }
 
+    public void lessInfo()
+    {
+        moreInfoPanel.SetActive(false);
     }
 }
